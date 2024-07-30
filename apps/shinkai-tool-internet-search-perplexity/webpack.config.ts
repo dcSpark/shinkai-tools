8f56c252c56dbd407d94b5d86e797cea4314b7a3
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-07-30 14:42:59 -0500

    fixed some warnings

--- apps/shinkai-tool-internet-search-perplexity/webpack.config.ts
+++ apps/shinkai-tool-internet-search-perplexity/webpack.config.ts
@@ -9,7 +9,10 @@ module.exports = composePlugins(withNx(), (config, { options, context }) => {
     config,
     withToolWebpackConfig({
       entry: path.join(__dirname, 'src/index.ts'),
-      outputPath: path.join(__dirname, '../../dist/apps/shinkai-tool-internet-search-perplexity'),
+      outputPath: path.join(
+        __dirname,
+        '../../dist/apps/shinkai-tool-internet-search-perplexity',
+      ),
       tsConfigFile: path.join(__dirname, 'tsconfig.app.json'),
     }),
     {
@@ -18,13 +21,67 @@ module.exports = composePlugins(withNx(), (config, { options, context }) => {
           os: require.resolve('os-browserify/browser'),
           tty: require.resolve('tty-browserify'),
           util: require.resolve('util/'),
+          process: require.resolve('process/browser'),
         },
       },
       plugins: [
         new webpack.ProvidePlugin({
           process: 'process/browser',
         }),
+        // Add IgnorePlugin to ignore dynamic requires
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^\.\/.*$/,
+          contextRegExp: /merge-deep\/node_modules\/clone-deep/,
+        }),
+        // Optionally, you can use ContextReplacementPlugin
+        new webpack.ContextReplacementPlugin(
+          /merge-deep\/node_modules\/clone-deep/,
+          (data: { dependencies: { critical?: boolean }[] }) => {
+            delete data.dependencies[0].critical;
+            return data;
+          },
+        ),
+        // ContextReplacementPlugin for playwright-extra
+        new webpack.ContextReplacementPlugin(
+          /playwright-extra/,
+          (data: { dependencies: { critical?: boolean }[] }) => {
+            if (data.dependencies[0]) {
+              delete data.dependencies[0].critical;
+            }
+            return data;
+          },
+        ),
+        // ContextReplacementPlugin for tsconfig-paths
+        new webpack.ContextReplacementPlugin(
+          /tsconfig-paths/,
+          (data: { dependencies: { critical?: boolean }[] }) => {
+            if (data.dependencies[0]) {
+              delete data.dependencies[0].critical;
+            }
+            return data;
+          },
+        ),
+         // ContextReplacementPlugin for tsconfig-paths filesystem.js
+         new webpack.ContextReplacementPlugin(
+          /tsconfig-paths\/lib\/filesystem/,
+          (data: { dependencies: { critical?: boolean }[] }) => {
+            if (data.dependencies[0]) {
+              delete data.dependencies[0].critical;
+            }
+            return data;
+          },
+        ),
+        // ContextReplacementPlugin for tsconfig-paths specific files
+        new webpack.ContextReplacementPlugin(
+          /tsconfig-paths\/lib\/match-path-async/,
+          (data: { dependencies: { critical?: boolean }[] }) => {
+            if (data.dependencies[0]) {
+              delete data.dependencies[0].critical;
+            }
+            return data;
+          },
+        ),
       ],
-    }
+    },
   );
 });

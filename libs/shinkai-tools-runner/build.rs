e64479e5a196d19d14086b119a951291ef7b3bb0
Alfredo Gallardo <agallardol@outlook.com>
2024-08-26 12:35:07 -0400

    - fix: build.rs

--- libs/shinkai-tools-runner/build.rs
+++ libs/shinkai-tools-runner/build.rs
@@ -1,10 +1,10 @@
 use std::env;
 use std::fs;
+use std::io::Write;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rerun-if-changed=resources/*");
-
+    println!("cargo:rerun-if-changed=shinkai-tools-runner-resources/*");
     let resources_path =
         Path::new(env!("CARGO_MANIFEST_DIR")).join("shinkai-tools-runner-resources");
     let target_path =
@@ -23,8 +23,8 @@ fn main() {
         target_path.join(shinkai_tools_backend_binary_name);
     println!("Source path: {:?}", shinkai_tools_backend_source_path);
     println!("Backend path: {:?}", shinkai_tools_backend_destination_path);
+    fs::create_dir_all(&target_path).unwrap();
     if resources_path.exists() {
-        fs::create_dir_all(&target_path).unwrap();
         fs::copy(
             shinkai_tools_backend_source_path,
             shinkai_tools_backend_destination_path.clone(),
@@ -32,7 +32,42 @@ fn main() {
         .unwrap();
         println!("Copied backend binary to target path");
     } else {
-        println!("Resources path does not exist");
+        println!("Resources path does not exist, fetching from internet");
+        let client = reqwest::blocking::Client::new();
+        let arch = if cfg!(target_os = "linux") {
+            "x86_64-unknown-linux-gnu"
+        } else if cfg!(target_os = "macos") {
+            "aarch64-apple-darwin"
+        } else if cfg!(target_os = "windows") {
+            "x86_64-pc-windows-msvc"
+        } else {
+            panic!("Unsupported target OS");
+        };
+        let url = format!("https://download.shinkai.com/shinkai-tools-backend/binaries/production/{}/{}/shinkai-tools-backend{}",
+            arch,
+            env!("CARGO_PKG_VERSION"),
+            if cfg!(windows) { ".exe" } else { "" }
+        );
+        let mut response = client
+            .get(&url)
+            .send()
+            .expect("Failed to download backend binary");
+        if !response.status().is_success() {
+            panic!("Failed to download backend binary: {}", response.status());
+        }
+        fs::create_dir_all(&resources_path).unwrap();
+        let mut file = fs::File::create(&shinkai_tools_backend_source_path)
+            .expect("Failed to create backend binary file");
+        response
+            .copy_to(&mut file)
+            .expect("Failed to write backend binary to file");
+        file.flush().expect("error writing data");
+        fs::copy(
+            shinkai_tools_backend_source_path,
+            shinkai_tools_backend_destination_path.clone(),
+        )
+        .unwrap();
+        println!("Downloaded and copied backend binary to target path");
     }
 
     if !cfg!(target_os = "windows") {

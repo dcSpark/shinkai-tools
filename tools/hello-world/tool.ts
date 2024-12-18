2287ab07cc5d73a31dbe14d64edda4494a0fea71
Guillermo Valin <contacto@guillermovalin.com>
2024-12-18 15:39:56 -0300

    Remove hello-world and hello-world-python tools and update smart-search MD5 hash in directory.json

--- tools/hello-world/tool.ts
+++
@@ -1,9 +0,0 @@
-type CONFIG = {};
-type INPUTS = {};
-type OUTPUT = {};
-
-export async function run(config: CONFIG, inputs: INPUTS): Promise<OUTPUT> {
-    const data = "Hello world from repository.";
-    
-    return { response: data };
-}

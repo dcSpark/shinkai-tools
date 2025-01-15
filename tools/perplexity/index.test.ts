1c2b6f1538b989b984dae9894f465252a3b12c66
Eddie <edward@dcspark.io>
2025-01-15 09:52:32 -0300

    revert file

--- tools/perplexity/index.test.ts
+++ tools/perplexity/index.test.ts
@@ -1,20 +1,20 @@
-// import { expect } from 'jsr:@std/expect/expect';
-// import { run } from './tool.ts';
+import { expect } from 'jsr:@std/expect/expect';
+import { run } from './tool.ts';
 
 
-// Deno.test({
-//   name: 'run',
-//   // Perplexity web scrapping is not working in CI (need to figure out why)
-//   ignore: Deno.env.get('CI') === 'true',
-//   fn: async () => {
-//     const run_result = await run(
-//       {
-//         chromePath: Deno.env.get('CHROME_PATH'),
-//       },
-//       {
-//         query: 'What is the meaning of life?',
-//       },
-//     );
-//     expect(run_result.response).toEqual(expect.any(String));
-//   },
-// });
+Deno.test({
+  name: 'run',
+  // Perplexity web scrapping is not working in CI (need to figure out why)
+  ignore: Deno.env.get('CI') === 'true',
+  fn: async () => {
+    const run_result = await run(
+      {
+        chromePath: Deno.env.get('CHROME_PATH'),
+      },
+      {
+        query: 'What is the meaning of life?',
+      },
+    );
+    expect(run_result.response).toEqual(expect.any(String));
+  },
+});

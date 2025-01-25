c0e4a96a6e5f55d076a5f4813739ab2e57fea905
Eddie <edward@dcspark.io>
2025-01-25 12:57:51 -0300

    Memory Test

--- scripts/tests/tool-memory.test.ts
+++ scripts/tests/tool-memory.test.ts
@@ -1,5 +1,5 @@
-const code = Deno.readTextFileSync('./tool.ts');
-const metadata = JSON.parse(Deno.readTextFileSync('./metadata.json'));
+const code = Deno.readTextFileSync(import.meta.dirname + '/../../tools/memory/tool.ts');
+const metadata = JSON.parse(Deno.readTextFileSync(import.meta.dirname + '/../../tools/memory/metadata.json'));
 
 type INPUTS = {
     data?: string;
@@ -10,20 +10,23 @@ type INPUTS = {
 const X_SHINKAI_TOOL_ID = `example-${Math.random().toString(36).substring(2, 15)}`;
 const X_SHINKAI_APP_ID = `run-${Math.random().toString(36).substring(2, 15)}`;
 
+const base_url = Deno.env.get('SHINKAI_NODE_ADDR') ?? 'http://localhost:9950';
+const token = Deno.env.get('BEARER_TOKEN') ?? 'debug';
+const llm_provider = Deno.env.get('INITIAL_AGENT_NAMES') ? (Deno.env.get('INITIAL_AGENT_NAMES')??'').split(',')[0] : 'llama3_1_8b';
 async function runCommandTest(parameters: INPUTS) {
-    const response = await fetch('http://localhost:9950/v2/code_execution', {
+    const response = await fetch(base_url + '/v2/code_execution', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer debug',
+            'Authorization': 'Bearer ' + token,
             'x-shinkai-tool-id': X_SHINKAI_TOOL_ID,
             'x-shinkai-app-id': X_SHINKAI_APP_ID,
-            'x-shinkai-llm-provider': 'o_qwen2_5_coder_latest',
+            'x-shinkai-llm-provider': llm_provider,
             'Content-Type': 'application/json; charset=utf-8',
         },
         body: JSON.stringify({
             code,
             tool_type: 'denodynamic',
-            llm_provider: 'o_qwen2_5_coder_latest',
+            llm_provider,
             tools: metadata.tools || [],
             parameters
         })
@@ -33,10 +36,10 @@ async function runCommandTest(parameters: INPUTS) {
     return data;
 }
 
-async function startTest() {
-    const expect = (data: any, message: string | undefined) => {
-        if (!data) throw Error('[Check failed] ' + message ?? '');
-        else console.log('[Check passed] ' + message ?? '');
+export async function memoryTest() {
+    const expect = (data: any, message: string | undefined, errorStr: string | undefined) => {
+        if (!data) throw Error('[Check failed] ' + (message ?? '') + ' ' + (errorStr ?? ''));
+        else console.log('[Check passed] ' + (message ?? ''));
     }
 
     const parametersA: INPUTS = {
@@ -44,7 +47,7 @@ async function startTest() {
     }
     const dataA= await runCommandTest(parametersA);
     // console.log('================================\nExpected: Chile\n', dataA.generalMemory);
-    expect(dataA.generalMemory.match(/Chile/i), 'Chile found');
+    expect(dataA.generalMemory.match(/Chile/i), '0. Chile found', JSON.stringify(dataA));
     
     const parametersB: INPUTS = {
         data: 'Argentina,[C] officially the Argentine Republic,[A][D] is a country in the southern half of South America. Argentina covers an area of 2,780,400 km2 (1,073,500 sq mi),[B] making it the second-largest country in South America after Brazil, the fourth-largest country in the Americas, and the eighth-largest country in the world. It shares the bulk of the Southern Cone with Chile to the west, and is also bordered by Bolivia and Paraguay to the north, Brazil to the northeast, Uruguay and the South Atlantic Ocean to the east, and the Drake Passage to the south. Argentina is a federal state subdivided into twenty-three provinces, and one autonomous city, which is the federal capital and largest city of the nation, Buenos Aires. The provinces and the capital have their own constitutions, but exist under a federal system. Argentina claims sovereignty over the Falkland Islands, South Georgia and the South Sandwich Islands, the Southern Patagonian Ice Field, and a part of Antarctica.',
@@ -52,8 +55,8 @@ async function startTest() {
    
     const dataB = await runCommandTest(parametersB);
     // console.log('================================\nExpected: Chile & Argentina\n', dataB.generalMemory);
-    expect(dataB.generalMemory.match(/Chile/i), 'Chile found');
-    expect(dataB.generalMemory.match(/Argentina/i), 'Argentina found');
+    expect(dataB.generalMemory.match(/Chile/i), '1. Chile found', JSON.stringify(dataB));
+    expect(dataB.generalMemory.match(/Argentina/i), '2. Argentina found', JSON.stringify(dataB));
     
     const parametersC: INPUTS = {
         data: 'Spain,[f] officially the Kingdom of Spain,[a][g] is a country in Southwestern Europe with territories in North Africa.[12][h] Featuring the southernmost point of continental Europe, it is the largest country in Southern Europe and the fourth-most populous European Union member state. Spanning across the majority of the Iberian Peninsula, its territory also includes the Canary Islands, in the Eastern Atlantic Ocean, the Balearic Islands, in the Western Mediterranean Sea, and the autonomous cities of Ceuta and Melilla, in Africa. Peninsular Spain is bordered to the north by France, Andorra, and the Bay of Biscay; to the east and south by the Mediterranean Sea and Gibraltar; and to the west by Portugal and the Atlantic Ocean. Spain\'s capital and largest city is Madrid, and other major urban areas include Barcelona, Valencia, Seville, Zaragoza, Málaga, Murcia and Palma de Mallorca.',
@@ -62,9 +65,9 @@ async function startTest() {
    
     const dataC = await runCommandTest(parametersC);
     // console.log('================================\nExpected: Spain\n', dataC.specificMemory);
-    expect(dataC.specificMemory.match(/Spain/i), 'Spain found');
-    expect(!dataC.specificMemory.match(/Chile/i), 'Chile not found');
-    expect(!dataC.specificMemory.match(/Argentina/i), 'Argentina not found');
+    expect(dataC.specificMemory.match(/Spain/i), '3. Spain found', JSON.stringify(dataC));
+    expect(!dataC.specificMemory.match(/Chile/i), '4. Chile not found', JSON.stringify(dataC));
+    expect(!dataC.specificMemory.match(/Argentina/i), '5. Argentina not found', JSON.stringify(dataC));
 
     const parametersD: INPUTS = {
         data: 'France,[X] officially the French Republic,[XI] is a country located primarily in Western Europe. Its overseas regions and territories include French Guiana in South America, Saint Pierre and Miquelon in the North Atlantic, the French West Indies, and many islands in Oceania and the Indian Ocean, giving it one of the largest discontiguous exclusive economic zones in the world. Metropolitan France shares borders with Belgium and Luxembourg to the north, Germany to the northeast, Switzerland to the east, Italy and Monaco to the southeast, Andorra and Spain to the south, and a maritime border with the United Kingdom to the northwest. Its metropolitan area extends from the Rhine to the Atlantic Ocean and from the Mediterranean Sea to the English Channel and the North Sea. Its eighteen integral regions—five of which are overseas—span a combined area of 643,801 km2 (248,573 sq mi) and have a total population of nearly 68.4 million as of January 2024. France is a semi-presidential republic with its capital in Paris, the country\'s largest city and main cultural and economic centre.',        // key: 'key'
@@ -72,29 +75,28 @@ async function startTest() {
     }
     const dataD = await runCommandTest(parametersD);
     // console.log('================================\nExpected: France\n', dataD.specificMemory);
-    expect(dataD.specificMemory.match(/France/i), 'France found');
+    expect(dataD.specificMemory.match(/France/i), '6. France found', JSON.stringify(dataD));
     
     const parametersE: INPUTS = {
     };
     const dataE = await runCommandTest(parametersE);
     // console.log('================================\nExpected: Chile & Argentina\n', dataE.generalMemory);
-    expect(dataE.generalMemory.match(/Chile/i), 'Chile found');
-    expect(dataE.generalMemory.match(/Argentina/i), 'Argentina found');
-    expect(!dataE.specificMemory.match(/Spain/i), 'Spain not found');
+    expect(dataE.generalMemory.match(/Chile/i), '7. Chile found', JSON.stringify(dataE));
+    expect(dataE.generalMemory.match(/Argentina/i), '8. Argentina found', JSON.stringify(dataE));
+    expect(!dataE.specificMemory.match(/Spain/i), '9. Spain not found', JSON.stringify(dataE));
    
     const parametersF: INPUTS = {
         key: 'spain'
     };
     const dataF = await runCommandTest(parametersF);
     // console.log('================================\nExpected: Spain\n', dataF.specificMemory);
-    expect(dataF.specificMemory.match(/Spain/i), 'Spain found');
+    expect(dataF.specificMemory.match(/Spain/i), '10. Spain found', JSON.stringify(dataF));
     
     const parametersG: INPUTS = {
         key: 'england'
     };
     const dataG = await runCommandTest(parametersG);
     // console.log('================================\nExpected: None\n', dataG.specificMemory);
-    expect(dataG.specificMemory === '' || dataG.specificMemory === null, 'Empty memory');
+    expect(dataG.specificMemory === '' || dataG.specificMemory === null, '11. Empty memory', JSON.stringify(dataG));
 }
 
-await startTest();
--- scripts/zip.test.ts
+++ scripts/zip.test.ts
@@ -3,6 +3,7 @@ import { getMetadata, processToolsDirectory, saveToolsInNode } from "./build_too
 import { join } from "https://deno.land/std/path/mod.ts";
 import { exists } from "https://deno.land/std/fs/mod.ts";
 import { stripVersion, systemTools } from "./build_tools/system.ts";
+import { memoryTest } from "./tests/tool-memory.test.ts";
 
 Deno.test("Check if related tools exist", async () => {
   const tools_raw = await processToolsDirectory();
@@ -59,3 +60,6 @@ Deno.test("Compare shinkai-node generated ZIP __tool.json vs .tool-dump.test.jso
 });
 
 
+Deno.test("Check if memory is working", async () => {
+  await memoryTest();
+});

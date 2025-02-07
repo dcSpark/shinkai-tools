9a713a77f11aaa807c302d65435def952b66cd50
Eduardo Soto <esotom92@gmail.com>
2025-02-07 13:52:16 -0300

    feat: add reddit-blog-post-generator tool

---
+++ tools/reddit-blog-post-generator/tool.ts
@@ -0,0 +1,58 @@
+import { shinkaiLlmPromptProcessor, redditScraper } from './shinkai-local-tools.ts'
+
+type Configurations = {
+  post_guidelines: string;
+}
+
+type Inputs = {
+  subreddit: string;
+  get_by: 'hot' | 'new' | 'top' | 'rising';
+  top_time: 'day' | 'week' | 'month' | 'year' | 'all';
+}
+
+type Output = {
+  post_title: string;
+  post_content: string; // Markdown blog post
+}
+
+export const run = async (config: Configurations, inputs: Inputs) => {
+  const {subreddit} = await redditScraper({
+    action: 'subreddit',
+    subreddit: inputs.subreddit,
+    get_by: inputs.get_by,
+    top_time: inputs.top_time
+  });
+  const posts = subreddit?.posts || [];
+  const postContexts: string[] = []
+  for (const post of posts.slice(0,4)) {
+    const postContext = await redditScraper({
+      action: 'post',
+      post_url: post.url
+    });
+    postContexts.push(`
+      ${JSON.stringify(postContext.post)}
+    `);
+  }
+  const llmResponse = await shinkaiLlmPromptProcessor({
+    prompt: `
+    Write a blog post about the following posts:
+    <posts>
+    ${postContexts.join('\n\n')}
+    </posts>
+    Please follow the following guidelines:
+    <guidelines>
+    ${config.post_guidelines}
+    </guidelines>
+    You must output the post title and the post content wrapped in <post_title> and <post_content> tags respectively.
+    You must write the post content in HTML format.
+    `,
+    format: 'text',
+  });
+  const post_title = llmResponse.message.match(/<post_title>(.*?)<\/post_title>/)?.[1] || '';
+  let post_content = llmResponse.message.match(/<post_content>(.*?)<\/post_content>/)?.[1] || '';
+  if (!post_content) post_content = llmResponse.message.split('<post_content>')[1]
+  return {
+    post_title,
+    post_content
+  };
+}
